5a8c6bcca4784073cf5ad8b2e0ccb9b38ca7866e
timmehtimmeh <timmehtimmeh+github@gmail.com>
2017-11-05 20:10:48 -0800

    Delete Install.bat

--- Install.bat
+++
@@ -1,4 +0,0 @@
-set CURA_DIR = C:\Program Files\Ultimaker Cura 3.0
-
-xcopy ./plugins/DremelGCodeWriter CURA_DIR/plugins
-xcopy ./resources/Definitions/Dremel3D20.def.json CURA_DIR/resources/definitions
