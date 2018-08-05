267411f1a917c6c4bbfb08dc3ce97ba84746ab16
timmeh <timmehtimmeh@gmail.com>
2018-08-04 17:53:30 -0700

    Adding "Report Issue" button to plugin to allow users to more easily report issues

--- plugins/Dremel3D20/Dremel3D20.py
+++ plugins/Dremel3D20/Dremel3D20.py
@@ -140,6 +140,7 @@ class Dremel3D20(QObject, MeshWriter, Extension):
         #    self.addMenuItem(catalog.i18nc("@item:inmenu", "Install Dremel3D20 Printer"), self.installPluginFiles)
 
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Preferences"), self.showPreferences)
+        self.addMenuItem(catalog.i18nc("@item:inmenu", "Report Issue"), self.reportIssue)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Help "), self.showHelp)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Dremel Printer Plugin Version "+Dremel3D20.version), self.openPluginWebsite)
 
@@ -181,6 +182,19 @@ class Dremel3D20(QObject, MeshWriter, Extension):
             message = Message(catalog.i18nc("@info:status", "Dremel 3D20 plugin could not open help document.\n Please download it from here: https://github.com/timmehtimmeh/Cura-Dremel-3D20-Plugin/raw/cura-3.4/README.pdf"))
             message.show()
         return
+
+    @pyqtSlot()
+    def reportIssue(self):
+        Logger.log("i", "Dremel 3D20 Plugin opening issue page: https://github.com/timmehtimmeh/Cura-Dremel-3D20-Plugin/issues/new")
+        try:
+            if not QDesktopServices.openUrl(QUrl("https://github.com/timmehtimmeh/Cura-Dremel-3D20-Plugin/issues/new")):
+                message = Message(catalog.i18nc("@info:status", "Dremel 3D20 plugin could not open https://github.com/timmehtimmeh/Cura-Dremel-3D20-Plugin/issues/new please navigate to the page and report an issue"))
+                message.show()
+        except:
+            message = Message(catalog.i18nc("@info:status", "Dremel 3D20 plugin could not open https://github.com/timmehtimmeh/Cura-Dremel-3D20-Plugin/issues/new please navigate to the page and report an issue"))
+            message.show()
+        return
+
 
     def oldVersionInstalled(self):
         cura_dir=os.path.dirname(os.path.realpath(sys.argv[0]))
