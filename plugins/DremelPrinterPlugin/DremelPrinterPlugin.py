616909835f8d831b2293aaef54f67b1b18aa1f4f
Tim Schoenmackers <tschoenmackers@intrepidautomation.com>
2025-11-20 15:20:41 -0800

    Fix #113 - Apparently Ultimaker changed the parameters passed into the meshwriter.write function

--- plugins/DremelPrinterPlugin/DremelPrinterPlugin.py
+++ plugins/DremelPrinterPlugin/DremelPrinterPlugin.py
@@ -25,6 +25,8 @@ import copy
 import struct
 
 from distutils.version import StrictVersion # for upgrade installations
+from io import StringIO, BufferedIOBase #To write the g-code to a temporary buffer, and for typing.
+from typing import cast, List
 
 from UM.i18n import i18nCatalog
 from UM.Extension import Extension
@@ -35,6 +37,7 @@ from UM.Mesh.MeshWriter import MeshWriter
 from UM.Settings.InstanceContainer import InstanceContainer
 from UM.Qt.Duration import DurationFormat
 from UM.PluginRegistry import PluginRegistry
+from UM.Scene.SceneNode import SceneNode #For typing.
 
 from UM.Application import Application
 from UM.Settings.InstanceContainer import InstanceContainer
@@ -631,7 +634,7 @@ class DremelPrinterPlugin(QObject, MeshWriter, Extension):
     ##  breakdown of the dremel g3drem file format see the following page:
     ##  https://github.com/metalman3797/Cura-Dremel-3D20-Plugin/blob/master/README.md#technical-details-of-the-g3drem-file-format
     ######################################################################
-    def write(self, stream, nodes, mode = MeshWriter.OutputMode.BinaryMode):
+    def write(self, stream: BufferedIOBase, nodes: List[SceneNode], mode = MeshWriter.OutputMode.BinaryMode, **kwargs) -> bool:
         try:
             if mode != MeshWriter.OutputMode.BinaryMode:
                 Logger.log("e", "Dremel Plugin does not support non-binary mode.")
